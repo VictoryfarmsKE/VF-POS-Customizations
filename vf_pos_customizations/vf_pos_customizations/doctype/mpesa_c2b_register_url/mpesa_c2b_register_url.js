ed9f8ebd0896c6f2170f460e6656a403305d45f2
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-07-13 22:29:35 +0300

    pos essential doctypes

---
+++ vf_pos_customizations/vf_pos_customizations/doctype/mpesa_c2b_register_url/mpesa_c2b_register_url.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Christine Kanga and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mpesa C2B Register URL", {
+// 	refresh(frm) {
+
+// 	},
+// });
